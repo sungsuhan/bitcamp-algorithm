4dbfe3ffa27477b67f500451aecf13293c25e31e
sungsuhan <swhanssu@gmail.com>
2022-06-12 23:58:12 +0900

    2022-06-12 23 null

--- src/main/java/kr/co/clozet/algorithm/_binSearch/부품찾기.java
+++ src/main/java/kr/co/clozet/algorithm/_binSearch/부품찾기.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 /**
  * 전자 매장에는 부품이 N개 있다. 각 부품은 정수 형태의 고유한 번호가 있다. 어느 날 손님이 M개 종류의 부품을 대량으로 구매
@@ -40,17 +40,6 @@ import java.util.Scanner;
  **/
 public class 부품찾기 {
 
-    @Builder @Getter @AllArgsConstructor @NoArgsConstructor
-    public static class Solution{
-        private int[] arr;
-        private int target;
-        private int start;
-        private int end;
-        @Override
-        public String toString(){
-            return "";
-        }
-    }
     public static int binarySearch(int[] arr, int target, int start, int end) {
         if (start > end) return -1;
 
@@ -65,40 +54,94 @@ public class 부품찾기 {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("가게의 부품 총 개수");
-        int n = sc.nextInt();
-        int[] components = new int[n];
-        sc.nextLine();
-        System.out.println(n+"개의 부품번호");
-        for (int i = 0; i < n; i++) {
-            components[i] = sc.nextInt();
-        }
-        sc.nextLine();
-        System.out.println("손님이 문의한 부품 총 개수");
-        int m = sc.nextInt();
-        int[] askedComp = new int[m];
-        sc.nextLine();
-        System.out.println(m+"개의 부품번호");
-        for (int i = 0; i < m; i++) {
-            askedComp[i] = sc.nextInt();
-        }
-
-        String[] results = new String[m];
-
-        // 정렬
-        Arrays.sort(components);
-
-        // 이진탐색
-        for (int i = 0; i < m; i++) {
-            if (binarySearch(components, askedComp[i], 0, n - 1) >= 0) {
-                results[i] = "YES";
-            } else results[i] = "NO";
-        }
-
-        for (String result : results) {
-            System.out.print(result + " ");
+    @Builder @Getter @AllArgsConstructor @NoArgsConstructor
+    public static class Solution{
+        int[] components;
+        int[] askedComp;
+        int n;
+        int m;
+        String[] result;
+        @Override
+        public String toString(){
+            return String.format("%s", result);
         }
     }
+
+    @FunctionalInterface
+    public interface  SolutionService{
+        Solution solution(Solution s);
+    }
+
+    static class Service {
+        Solution test(Solution s) {
+            SolutionService f = e -> {
+                int[] components = new int[e.getN()];
+                int[] askedComp = new int[e.getM()];
+                String[] result = new String[e.getM()];
+                Arrays.sort(e.getComponents());
+                for (int i=0; i<e.getM(); i++) {
+                    if (binarySearch(components, askedComp[i], 0, e.getN() - 1) >= 0) {
+                        result[i] = "YES";
+                    } else result[i] = "NO";
+                }
+                return Solution.builder()
+                        .components(e.getComponents())
+                        .askedComp(e.getAskedComp())
+                        .result(e.getResult())
+                        .build();
+            };
+            return f.solution(s);
+        }
+    }
+    @Test
+    public void testSolution(){
+        int[] components = {8, 3, 7, 9, 2};
+        int[] askedComp = {5, 7, 9};
+        int n = 5;
+        int m = 3;
+        Solution s = Solution.builder()
+                .components(components)
+                .askedComp(askedComp)
+                .n(n)
+                .m(m)
+                .build();
+        Service s2 = new Service();
+        System.out.println(s2.test(s));
+    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("가게의 부품 총 개수");
+//        int n = sc.nextInt();
+//        int[] components = new int[n];
+//        sc.nextLine();
+//        System.out.println(n+"개의 부품번호");
+//        for (int i = 0; i < n; i++) {
+//            components[i] = sc.nextInt();
+//        }
+//        sc.nextLine();
+//        System.out.println("손님이 문의한 부품 총 개수");
+//        int m = sc.nextInt();
+//        int[] askedComp = new int[m];
+//        sc.nextLine();
+//        System.out.println(m+"개의 부품번호");
+//        for (int i = 0; i < m; i++) {
+//            askedComp[i] = sc.nextInt();
+//        }
+//
+//        String[] results = new String[m];
+//
+//        // 정렬
+//        Arrays.sort(components);
+//
+//        // 이진탐색
+//        for (int i = 0; i < m; i++) {
+//            if (binarySearch(components, askedComp[i], 0, n - 1) >= 0) {
+//                results[i] = "YES";
+//            } else results[i] = "NO";
+//        }
+//
+//        for (String result : results) {
+//            System.out.print(result + " ");
+//        }
+//    }
 }
--- src/main/java/kr/co/clozet/algorithm/_sort/K번째수.java
+++ src/main/java/kr/co/clozet/algorithm/_sort/K번째수.java
@@ -1,5 +1,13 @@
 package kr.co.clozet.algorithm._sort;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 /**
  * packageName:kr.co.clozet.algorithm._sort
  * fileName        :K번째수.java
@@ -12,4 +20,63 @@ package kr.co.clozet.algorithm._sort;
  * 2022-05-25           sungsuhan      최초 생성
  **/
 public class K번째수 {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Solution{
+        private int[] array;
+        int[][] commands;
+        private int[] answer;
+
+        @Override
+        public String toString() {
+            return String.format("array: %s\ncommands: %s\nreturn: %s\n",
+                    Arrays.toString(array), Arrays.deepToString(commands), Arrays.toString(answer));
+        }
+    }
+
+    @FunctionalInterface
+    public interface SolutionService {
+        Solution solution(Solution s);
+    }
+
+    class Service {
+        Solution test(Solution s) {
+            SolutionService f = e -> {
+                int[] answer = new int[3];
+                int answerIndex = 0;
+                for (int[] command : e.getCommands()) {
+                    int[] temp = new int[command[1] - command[0] + 1];
+                    int tempIndex = 0;
+                    for (int i = command[0]; i <= command[1]; i++) {
+                        temp[tempIndex++] = e.getArray()[i - 1];
+                    }
+                    Arrays.sort(temp);
+                    answer[answerIndex++] = temp[command[2] - 1];
+                }
+                return Solution.builder()
+                        .array(e.getArray())
+                        .commands(e.getCommands())
+                        .answer(answer)
+                        .build();
+            };
+            return f.solution(s);
+        }
+    }
+
+    @Test
+    void testSolution(){
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
+        Solution s = Solution.builder()
+                .array(array)
+                .commands(commands)
+                .build();
+        Service s2 = new Service();
+        System.out.println(s2.test(s));
+
+
+    }
+
 }
