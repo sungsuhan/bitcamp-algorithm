9c3e7fd0fa115376d3bc68115f284739cb1ec5c1
sungsuhan <swhanssu@gmail.com>
2022-06-15 11:13:45 +0900

    11 체육복

--- src/main/java/kr/co/clozet/algorithm/_dfsBfs/네트워크.java
+++ src/main/java/kr/co/clozet/algorithm/_dfsBfs/네트워크.java
@@ -1,6 +1,27 @@
 package kr.co.clozet.algorithm._dfsBfs;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+
 /**
+ * 네트워크란 컴퓨터 상호 간에 정보를 교환할 수 있도록 연결된 형태를 의미합니다. 예를 들어, 컴퓨터 A와 컴퓨터 B가 직접적으로
+ * 연결되어있고, 컴퓨터 B와 컴퓨터 C가 직접적으로 연결되어 있을 때 컴퓨터 A와 컴퓨터 C도 간접적으로 연결되어 정보를 교환할
+ * 수 있습니다. 따라서 컴퓨터 A, B, C는 모두 같은 네트워크 상에 있다고 할 수 있습니다.
+ * 컴퓨터의 개수 n, 연결에 대한 정보가 담긴 2차원 배열 computers 가 매개변수로 주어질 때, 네트워크의 개수를 return 하도록
+ * solution 함수를 작성하시오.
+ * <제한사항>
+ * 컴퓨터의 개수 n은 1이상 200이하인 자연수
+ * 각 컴퓨터는 0부터 n-1 인 정수로 표현
+ * i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computers[i][j]를 1로 표현
+ * computer[i][i]는 항상 1
+ * <입출력 예시>
+ * n              computer                    return
+ * 3    [[1,1,0], [1,1,0], [0,0,1]]             2
+ * 3    [[1,1,0], [1,1,1], [0,1,1]]             1
+ *
  * packageName:kr.co.clozet.algorithm._dfsBfs
  * fileName        :네트워크.java
  * author          : sungsuhan
@@ -12,4 +33,48 @@ package kr.co.clozet.algorithm._dfsBfs;
  * 2022-05-25           sungsuhan      최초 생성
  **/
 public class 네트워크 {
+//    @Builder @Getter @AllArgsConstructor @NoArgsConstructor
+//    public static class Solution{
+//
+//        @Override
+//        public String toString(){
+//            return String.format("");
+//        }
+//    }
+//
+//    @FunctionalInterface
+//    public interface SolutionService{
+//        Solution solution(Solution S);
+//    }
+//
+//    class Service{
+//        Solution test(Solution s) {
+//            SolutionService f = e -> {
+//
+//                return Solution.builder().build();
+//            };
+//            return f.solution(s);
+//        }
+//    }
+//
+//    @Test
+//    public void testSolution(){
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
 }
--- src/main/java/kr/co/clozet/algorithm/_greedy/체육복.java
+++ src/main/java/kr/co/clozet/algorithm/_greedy/체육복.java
@@ -1,6 +1,34 @@
 package kr.co.clozet.algorithm._greedy;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
+ * 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다.
+ * 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다.
+ * 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에
+ * 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+ * 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열
+ * reserve 가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+ * <제한사항>
+ * 전체 학생의 수는 2명 이상 30명 이하입니다.
+ * 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+ * 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+ * 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+ * 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은
+ * 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+ * <입출력 예>
+ * n    lost    reserve     return
+ * 5    [2,4]   [1,3,5]       5
+ * 5    [2,4]     [3]         4
+ * 3     [3]      [1]         2
+ *
+ *
  * packageName:kr.co.clozet.algorithm._greedy
  * fileName        :체육복.java
  * author          : sungsuhan
@@ -12,4 +40,69 @@ package kr.co.clozet.algorithm._greedy;
  * 2022-05-25           sungsuhan      최초 생성
  **/
 public class 체육복 {
+    @Builder @Getter @AllArgsConstructor @NoArgsConstructor
+    public static class Solution{
+        int n;
+        int[] lost;
+        int[] reserve;
+        int answer;
+
+        @Override
+        public String toString(){
+            return String.format("%d", answer);
+        }
+    }
+
+    @FunctionalInterface
+    public interface SolutionService{
+        Solution solution(Solution S);
+    }
+
+    class Service{
+        Solution test(Solution s) {
+            SolutionService f = e -> {
+                e.answer = e.n;
+                int[] student = new int[e.n];
+
+                Arrays.fill(student, 1);
+                for (int i : e.lost)
+                    student[i-1] -= 1;
+                for (int i : e.reserve)
+                    student[i-1] += 1;
+                for (int i=0; i<student.length; i++) {
+                    if (student[i]==0) {
+                        if (i>0 && student[i-1] == 2)
+                            student[i-1] -= 1;
+                        else if (i<e.n-1 && student[i+1]==2)
+                            student[i+1] -= 1;
+                        else
+                            e.answer--;
+                    }
+                }
+                return Solution.builder()
+                        .lost(e.getLost())
+                        .reserve(e.getReserve())
+                        .answer(e.getAnswer())
+                        .build();
+            };
+            return f.solution(s);
+        }
+    }
+
+    @Test
+    public void testSolution(){
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        Solution s = Solution.builder()
+                .n(n)
+                .lost(lost)
+                .reserve(reserve)
+                .build();
+        Service s2 = new Service();
+        System.out.println(s2.test(s));
+    }
+
+
+
 }
